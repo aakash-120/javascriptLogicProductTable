4fe90150db5f113966ae09a15d7536a6bd8e845f
aakash_vishwakarma <aakashvishwakarma@cedcoss.com>
2022-02-08 14:45:06 +0530

    product table through object

--- src/index.js
+++ src/index.js
@@ -1,23 +1,48 @@
+var products = []
+
 function addRow()
 {
     
-    var fname = document.getElementById('fname').value;
-     var lname = document.getElementById('lname').value;
-      var age = document.getElementById('age').value;
-      
-     
-      var table = document.getElementsByTagName('table')[0];
-      
+    var id = document.getElementById('p_id').value;
+     var name = document.getElementById('p_name').value;
+      var price = document.getElementById('p_price').value;
     
-      var newRow = table.insertRow(table.rows.length/2+1);
+      var eachProduct = {};
+      eachProduct.id = id;
+      eachProduct.name = name;
+      eachProduct.price = price;
+
+    if(isExist(id) == false)
+    {
+        products.push(eachProduct);
+    }
+
+    //  products.push(eachProduct);
+      display();
+      console.log("products",products);
       
-    
-      var cel1 = newRow.insertCell(0);
-      var cel2 = newRow.insertCell(1);
-      var cel3 = newRow.insertCell(2);
-      
-     
-      cel1.innerHTML = fname;
-      cel2.innerHTML = lname;
-      cel3.innerHTML = age;
+}
+
+function display() {
+    var html = '';
+    html += '<table><tr><th>ID</th><th>NAME</th><th>PRICE</th></tr>';
+    for(i =0 ; i < products.length ; i++)
+    {
+        html += '<tr><td>'+products[i].id+'</td><td>'+products[i].name+'</td><td>'+products[i].price+'</td></tr>';
+
+    }
+    html += '</table';
+    document.getElementById('result').innerHTML = html;
+}
+
+function isExist(id) {
+    for(i =0 ; i < products.length ; i++)
+    {
+        if(products[i].id == id)
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
